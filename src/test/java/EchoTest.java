dfad742a882af4233925bdc62eea1ef7966b1e27
Oleg <olegkajlev@gmail.com>
2023-08-13 08:26:21 +0300

    second commit

--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -14,7 +14,7 @@ public class EchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("so data"));
+                .body("data", equalTo("some data"));
     }
 
 
